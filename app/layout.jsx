acb81d22efcc146fdec50647ba40d09f6b1b8f9a
matt1013 <34205231+matt1013@users.noreply.github.com>
2024-04-13 12:56:51 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-yellow-200">
+            <body className="antialiased text-white bg-yellow-600">
                 <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
